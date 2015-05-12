d268345dcf6fef8dfcd18124697350b9f2797b13
team18d <felisa93@freemail.hu>
2015-05-12 17:44:46 +0200

    highFive3

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "Graph.h"
+#include "../Algorithms/Graph.h"
+#include "../Algorithms/Source.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -9,24 +10,33 @@ namespace UnitTest1
 {
 	TEST_CLASS(UnitTest1)
 	{
+#define TEST_CASE_DIRECTORY GetDirectoryName(__FILE__)
+
+		string GetDirectoryName(string path){
+			const size_t last_slash_idx = path.rfind('\\');
+			if (std::string::npos != last_slash_idx)
+			{
+				return path.substr(0, last_slash_idx + 1);
+			}
+			return "";
+		}
 	public:
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestMethodReadZero)
 		{
 			Graph g;
 			int ret = g.read("0");
 			Assert::AreEqual(ret, -1);
 		}
 
-		TEST_METHOD(TestMethod2)
+		TEST_METHOD(TestMethodReadOne)
 		{
-			/*string fileName = "./TestFiles/test0.txt";
-
 			Graph g;
-			int ret = g.read(fileName);
-			Assert::AreEqual(ret, -1);*/
-
+			string filename = std::string(TEST_CASE_DIRECTORY) + "test1.txt";
+			int ret = g.read(filename);
+			Assert::AreEqual(ret, 0);
 		}
+
 
 	};
 }
